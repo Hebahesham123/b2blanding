27ad587b807c7ac480c2fd0543d7cce31d87fa46
Hebahesham123 <149805116+Hebahesham123@users.noreply.github.com>
2026-02-12 11:53:05 +0200

    Add files via upload

--- src/app/api/submit/route.ts
+++ src/app/api/submit/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
-const PROMO_CODE = "NS4020";
+const PROMO_CODE = "NS10";
 
 export async function POST(request: Request) {
   try {
